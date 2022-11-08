055f83e7269f866c552c36b59da443073ab3d147
ASUS <batyrkhanov95@gmail.com>
2022-11-08 20:41:46 +0600

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,5 +3,6 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("08.11.2022");
+        System.out.println("Случайно нажал commit,вместо commit and push. Хотел повторно нажать галочку, но не получается без изменений");
     }
 }
