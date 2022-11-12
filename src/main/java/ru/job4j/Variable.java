450db108a12af1640ca8271295655586f465c2c8
ASUS <batyrkhanov95@gmail.com>
2022-11-12 13:32:19 +0600

    3. Арифметические операции. [#185]

---
+++ src/main/java/ru/job4j/Variable.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class Variable {
+    public static void main(String[] args) {
+        byte vozrast = 25;
+        char pol = 'm';
+        float rost = 1.71F;
+        int domkirpich = 15300;
+        long bakteria = 1000000000;
+        double kosmos = 149597870.7;
+        boolean razreshenienarabotu = true;
+        double razmerfaila = 124578.4;
+        int result = domkirpich - vozrast;
+        System.out.println(result);
+    }
+
+}
