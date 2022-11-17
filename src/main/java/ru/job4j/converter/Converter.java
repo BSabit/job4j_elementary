bf6dd251e18f2f9b213f77760d92d0f7e627f783
ASUS <batyrkhanov95@gmail.com>
2022-11-17 17:29:03 +0600

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -16,16 +16,30 @@ public class Converter {
         return rsl;
     }
 
+    public static float euroToTenge(float euro) {
+        float rsl = euro * 477;
+        return rsl;
+    }
+
+    public static float dollarToTenge(float dollar) {
+        float rsl = dollar * 461;
+        return rsl;
+    }
+
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float euroTenge1 = Converter.tengeToEuro(4770);
-        float euroTenge2 = Converter.tengeToEuro(2700);
-        float dollarTenge = Converter.tengeToDollar(4610);
+        float tengeEuro1 = Converter.tengeToEuro(4770);
+        float tengeEuro2 = Converter.tengeToEuro(2700);
+        float tengeDollar = Converter.tengeToDollar(4610);
         float dollar = Converter.tengeToDollar(1950);
+        float euroTenge = Converter.euroToTenge(15);
+        float dollarTenge = Converter.dollarToTenge(27);
         System.out.println("140 ruble are " + euro + " euro.");
-        System.out.println("4770 tenge are " + euroTenge1 + " euro.");
-        System.out.println("2700 tenge are " + euroTenge2 + " euro.");
-        System.out.println("4610 tenge are " + dollarTenge + " dollar.");
+        System.out.println("4770 tenge are " + tengeEuro1 + " euro.");
+        System.out.println("2700 tenge are " + tengeEuro2 + " euro.");
+        System.out.println("4610 tenge are " + tengeDollar + " dollar.");
         System.out.println("1950 tenge are " + dollar + " dollar.");
+        System.out.println("15 euro are " + euroTenge + " tenge.");
+        System.out.println("27 dollar are " + dollarTenge + " tenge.");
     }
 }
